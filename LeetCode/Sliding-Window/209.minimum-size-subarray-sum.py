fce9a5347fd5307107149c6b040a06889f0c613f
Jay <jayatbkrs@gmail.com>
2025-10-03 14:33:51 -0700

    209 minimum size of subarray sum solved

--- LeetCode/Sliding-Window/209.minimum-size-subarray-sum.py
+++ LeetCode/Sliding-Window/209.minimum-size-subarray-sum.py
@@ -1,17 +1,34 @@
-def minSubArrayLen(target, nums):
-    
-    min_len = float('inf')
-    curr_sum = 0
-    left = 0
-    n = len(nums)
-    
-    for right in range(n):
-        curr_sum += nums[right]
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        # min length of subarray whose sum is greater than or equal to target
 
-        while curr_sum >= target : 
-            min_len = min(min_len, right - left +1)
+        # we need a variable for min length, set it as inf value to whichever comes, it updates
+        min_length = float('inf')
+        
+        # left pointer 
+        left = 0
+        
+        # length of array
+        n = len(nums)
 
-            curr_sum -= nums[left]
-            left += 1
+        # curr_sum variable to store current sum value
+        curr_sum = 0
 
-    return min_len if min_len != float('inf') else 0
+        # will iterate nums using for loop and index as right pointer
+        for right in range(n):
+            # expand window until curr_sum is greater or equal to target
+            curr_sum += nums[right]
+
+            # in while curr_sum is greater than or equal to target
+            while curr_sum >= target:
+                # update current window size
+                window = (right - left) + 1
+                min_length = min(min_length, window)
+                
+                #then substract left pointer value from curr_sum
+                curr_sum -= nums[left]
+                # and move left pointer + 1
+                left += 1
+        
+        # return min_length if updated, else return 0
+        return min_length if min_length != float('inf') else 0
