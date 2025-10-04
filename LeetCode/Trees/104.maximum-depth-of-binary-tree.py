fb440b6cbfb37a52893ca5505c2adc9a39a1dfb8
Jay <jayatbkrs@gmail.com>
2025-10-04 11:04:16 -0700

    maximum depth of binary tree

--- LeetCode/Trees/104.maximum-depth-of-binary-tree.py
+++ LeetCode/Trees/104.maximum-depth-of-binary-tree.py
@@ -6,12 +6,16 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # edge case handling 
+        # Maximum Depth of Binary Tree -> DFS, Depth First Search
+
+        # in order to get it, we need to get each depth on root.left and root.right using DFS(recuisve)
+        # then will return 1 (depth itself) + max value between left and right, keep doing the same work
+
+        # base/edge case, if not root, then just return 0
         if not root:
             return 0
         
-        # return 1 (root) + maxDepth compared value from call root.left and root.left recursively
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-
+    
 # T: O(n)
 # S: O(h), worst case O(n)
