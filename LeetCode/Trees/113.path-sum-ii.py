17caf1b45012ec775b2f64f1eaa9d923ac831959
Jay <jayatbkrs@gmail.com>
2025-10-21 22:25:00 -0700

    113 path sum updated

--- LeetCode/Trees/113.path-sum-ii.py
+++ LeetCode/Trees/113.path-sum-ii.py
@@ -6,40 +6,29 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        
-        # get empty array to store path and return
         res = []
 
-        # get helper function taking node, remaining_sum, current_path for dfs 
-        def dfs(node, remaining_sum, curret_path):
-            # edge case
-            if not node:
+
+        def dfsR(root, path, total):
+            if not root: 
                 return 
-            
-            # append node.val to current path to track 
-            current_path.append(node.val)
 
-            # subtract node.val from remaining sum
-            remaining_sum -= node.val 
+            path.append(root.val)
+            total += root.val
 
-            # if all searched left and right and remaining sum == 0: 
-            if not node.right and node.left and remaining_sum == 0:
-                res.append(current_path[:]) # then append current_path deep copy to res
+            if not root.left and not root.right and total == targetSum:
+                res.append(list(path))
 
-            # call dfs until it gets to leap which the end from left 
-            if node.left:
-                dfs(node.left, remaining_sum, current_path)
-            if node.right:
-                dfs(node.left, remaining_sum, current_path)
+            dfsR(root.right, path, total)
+            dfsR(root.left, path, total)
 
-            current_path.pop() # if it got to the leap, then pop to go back which is backtracking
+            path.pop()
 
-        
-        # call the function to start
-        dfs(root, targetSum, [])
 
-        # return res 
-        return result
+        dfsR(root, [], 0)        
+
+        return res
+
     
 # T: O(n)
-# S: O(n)
+# S: O(h)
