5192f21369ee2899b4c8ee81eb228c085a9c8362
Jay <jayatbkrs@gmail.com>
2025-10-23 22:39:04 -0700

    subset solved

---
+++ LeetCode/78.subset.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res, curr = [], []
+
+        def dfs(i):
+            if i == len(nums):
+                res.append(curr[:])
+                return 
+
+            dfs(i + 1)
+
+            curr.append(nums[i])
+
+            dfs(i + 1)
+
+            curr.pop()
+
+        dfs(0)
+
+        return res
+    
+# T: O(n^2)
+# S: O(n)
